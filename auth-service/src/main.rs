55e73899d3eaa130cc1206eb43da647fcf6e8f0d
Riccardo Persiani <r.persiani92@gmail.com>
2026-01-29 12:28:50 +0100

    [Sprint 1][Task 1] fix: add correct Docker Hub username to compose.yml

--- auth-service/src/main.rs
+++ auth-service/src/main.rs
@@ -19,5 +19,5 @@ async fn main() {
 
 async fn hello_handler() -> Html<&'static str> {
     // TODO: Update this to a custom message!
-    Html("<h1>Hello, World!</h1>")
+    Html("<h1>Hello, World! This is Riccardo's first service!</h1>")
 }
