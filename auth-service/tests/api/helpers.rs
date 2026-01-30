2dac9815719a554a4acd0fa04672eaeb201b4b3e
Riccardo Persiani <r.persiani92@gmail.com>
2026-01-30 12:08:50 +0100

    [Sprint 1][Task 6] signup route interface

--- auth-service/tests/api/helpers.rs
+++ auth-service/tests/api/helpers.rs
@@ -1,5 +1,6 @@
 use auth_service::Application;
 use serde_json::json;
+use uuid::Uuid;
 
 pub struct TestApp {
     pub address: String,
@@ -36,18 +37,22 @@ impl TestApp {
             .expect("Failed to execute request.")
     }
 
-    pub async fn post_signup(&self) -> reqwest::Response {
+    pub async fn post_signup<Body>(&self, body: &Body) -> reqwest::Response
+    where
+        Body: serde::Serialize,
+    {
         self.http_client
             .post(&format!("{}/signup", &self.address))
-            .json(&json!({
-                "email": "user@example.com",
-                "password": "string",
-                "requires2FA": true
-            }))
+            .json(body)
             .send()
             .await
             .expect("Failed to execute request.")
     }
+
+    pub fn get_random_email() -> String {
+        format!("{}@example.com", Uuid::new_v4())
+    }
+
     pub async fn post_login(&self) -> reqwest::Response {
         self.http_client
             .post(&format!("{}/login", &self.address))
--- auth-service/tests/api/signup.rs
+++ auth-service/tests/api/signup.rs
@@ -4,8 +4,48 @@ use crate::helpers::TestApp;
 async fn signup_returns_auth_ui() {
     let app = TestApp::new().await;
 
-    let response = app.post_signup().await;
+    let random_email = TestApp::get_random_email();
+
+    let body = serde_json::json!({
+        "email": random_email,
+                    "password": "password123",
+            "requires2FA": true
+    });
+
+    let response = app.post_signup(&body).await;
 
     assert_eq!(response.status().as_u16(), 200);
     // assert_eq!(response.headers().get("content-type").unwrap(), "text/html");
+}
+
+#[tokio::test]
+async fn should_return_422_if_malformed_input() {
+    let app = TestApp::new().await;
+
+    let random_email = TestApp::get_random_email(); // Call helper method to generate email
+
+    let test_cases = [
+        serde_json::json!({
+            "password": "password123",
+            "requires2FA": true
+        }),
+        serde_json::json!({
+            "email": random_email,
+            "requires2FA": true
+        }),
+        serde_json::json!({
+            "email": random_email,
+            "password": "password123",
+        }),
+    ];
+
+    for test_case in test_cases.iter() {
+        let response = app.post_signup(test_case).await; // call `post_signup`
+        assert_eq!(
+            response.status().as_u16(),
+            422,
+            "Failed for input: {:?}",
+            test_case
+        );
+    }
 }
