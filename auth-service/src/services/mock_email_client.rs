b5779a300a4eff70270f6a230855675085b0d848
Riccardo Persiani <r.persiani92@gmail.com>
2026-03-27 11:57:12 +0100

    [Sprint 6][Task 3]: fix mock email client

--- auth-service/src/services/mock_email_client.rs
+++ auth-service/src/services/mock_email_client.rs
@@ -6,13 +6,8 @@ pub struct MockEmailClient;
 #[async_trait::async_trait]
 impl EmailClient for MockEmailClient {
     #[tracing::instrument(name = "Sending email", skip_all)]
-    async fn send_email(
-        &self,
-        recipient: &Email,
-        subject: &str,
-        content: &str,
-    ) -> Result<(), String> {
-        println!(
+    async fn send_email(&self, recipient: &Email, subject: &str, content: &str) -> Result<()> {
+        tracing::debug!(
             "Sending email to {} with subject: {} and content: {}",
             recipient.as_ref(),
             subject,
